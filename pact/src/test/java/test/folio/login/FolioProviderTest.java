510a7ac522348a9271ef4509d6a2d5d66d737e99
Igor_Gorchakov <Igor_Gorchakov@epam.com>
2018-08-28 18:44:47 +0300

    Remove test files, only test for FOLIO is left

---
+++ pact/src/test/java/test/folio/login/FolioProviderTest.java
@@ -0,0 +1,35 @@
+package test.folio.login;
+
+import au.com.dius.pact.provider.junit.PactRunner;
+import au.com.dius.pact.provider.junit.Provider;
+import au.com.dius.pact.provider.junit.State;
+import au.com.dius.pact.provider.junit.loader.PactFolder;
+import au.com.dius.pact.provider.junit.target.HttpTarget;
+import au.com.dius.pact.provider.junit.target.Target;
+import au.com.dius.pact.provider.junit.target.TestTarget;
+import org.junit.runner.RunWith;
+
+/**
+ * Test interacts with OKAPI proxy and calls mod-login using a mock client based on the contract.
+ * The communication contract powered by pact file.
+ *
+ * @author Igor Gorchakov
+ */
+@RunWith(PactRunner.class)
+@Provider("Provider")
+@PactFolder("src/test/resources/pacts/folio/login")
+public class FolioProviderTest {
+
+    /**
+     * Define the target to be used for verifying the interactions in the contract
+     */
+    @TestTarget
+    public final Target target = new HttpTarget("http", "localhost", 9130);
+
+    /**
+     * Specify the state in the contract that we want to test
+     */
+    @State("test GET")
+    public void toGetState() {
+    }
+}
